8537f40b8aab965a42592d63f64cefc6fdd9bfcc
shimada_hiroki <hiroki_shimada@sumzap.co.jp>
2017-04-06 12:23:48 +0900

    add dein

--- setup.sh
+++ setup.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
+# 通常のドットファイルを定義
 DOT_FILES=(.zshrc)
 
+# ホームディレクトリ配下にシンボリックリンクをはる
 for file in ${DOT_FILES[@]}
 do
-    ln -sf $HOME/dotfiles/$file $HOME/$file
+    ln -sf ~/dotfiles/$file ~/$file
 done
 
+# vimのプラグインパッケージ管理用のフォルダ作成
+if [ ! -d ~/.dein ]; then
+    mkdir ~/.dein
+fi
+
+# deinの設定ファイルをシンボリックリンク化
+ln -sf ~/dotfiles/.dein/dein.toml ~/.dein/dein.toml
+ln -sf ~/dotfiles/.dein/dein_lazy.toml ~/.dein/dein_lazy.toml
