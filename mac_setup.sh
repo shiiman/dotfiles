8875aa2cb83af151f3a5f1029609c9938c215231
shimada_hiroki <hiroki_shimada@sumzap.co.jp>
2017-04-07 10:42:15 +0900

    brewfiles更新

--- mac_setup.sh
+++ mac_setup.sh
@@ -2,12 +2,12 @@
 
 # Homebrewのインストール
 if ! type brew >/dev/null 2>&1; then
-	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
 # gitから設定ファイルをclone
 if ! type git >/dev/null 2>&1; then
-	brew install git
+    brew install git
 fi
 git clone https://github.com/bayguh/dotfiles.git ~/dotfiles
 
@@ -17,11 +17,14 @@ cd ~/dotfiles
 brew tap Homebrew/bundle
 # アプリインストール
 brew bundle
+
 # dotfileの設定
 sh ~/dotfiles/dotfile_setup.sh
+
 # zshをシェルリストに追加
 sudo sh -c "echo /usr/local/bin/zsh >> /etc/shells"
 # デフォルトシェルをzshに変更
 chsh -s /usr/local/bin/zsh
+
 # finderで隠しファイルの表示
 defaults write com.apple.finder AppleShowAllFiles TRUE
