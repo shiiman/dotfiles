2696d5c4cac7266aa6f2133f1bc1d414488727a6
hiroki_shimada <hsnonsense5@gmail.com>
2017-04-20 20:50:01 +0900

    ad java

--- anyenv_setup.sh
+++ anyenv_setup.sh
@@ -15,6 +15,7 @@ anyenv install rbenv
 anyenv install phpenv
 anyenv install ndenv
 anyenv install goenv
+anyenv install jenv
 
 echo 'export PATH="$HOME/.anyenv/bin:$PATH"' >> /tmp/anyenv.setting
 echo 'eval "$(anyenv init -)"' >> /tmp/anyenv.setting
@@ -30,12 +31,18 @@ phpenv install 7.1.4
 ndenv install v7.9.0
 goenv install 1.8.1
 
+jenv add $(/usr/libexec/java_home -v 1.6)
+jenv add $(/usr/libexec/java_home -v 1.7)
+jenv add $(/usr/libexec/java_home -v 1.8)
+
+
 # 各言語のバージョン反映
 pyenv global 3.6.1
 rbenv global 2.4.1
 phpenv global 7.1.4
 ndenv global v7.9.0
 goenv global 1.8.1
+jenv global 1.8
 
 # シェルの再読込
 source /tmp/anyenv.setting
