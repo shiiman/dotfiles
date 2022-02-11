0df97d3ad9f3b7662968ec3865e24b2b4e04914f
shiiman <hsnonsense5@gmail.com>
2022-02-11 12:45:41 +0900

    update brewfile

--- mac_setup.sh
+++ mac_setup.sh
@@ -2,7 +2,7 @@
 
 # Homebrewのインストール.
 if ! type brew >/dev/null 2>&1; then
-    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
 # GitHubから設定ファイルをclone.
