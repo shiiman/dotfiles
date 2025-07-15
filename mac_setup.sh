27b4df867e04abbacd9a7b427bac2c4ba927e6f4
shiiman <hsnonsense5@gmail.com>
2025-07-15 13:36:17 +0900

    update mac_setup.sh

--- mac_setup.sh
+++ mac_setup.sh
@@ -2,7 +2,7 @@
 
 # Homebrewのインストール.
 if ! type brew >/dev/null 2>&1; then
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     # パスを明示的に通す
     if [ -d /opt/homebrew/bin ]; then
         export PATH="/opt/homebrew/bin:$PATH"
